cb7469092742a1b794666aacf18e8838f972f47c
Joe Gallagher <joedgallagher@yahoo.com>
2018-12-08 16:04:05 -0500

    Added mindmap

---
+++ MindMap.mm
@@ -0,0 +1,102 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Site" FOLDED="false" ID="ID_129424494" CREATED="1544146220153" MODIFIED="1544146240280" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Author" POSITION="right" ID="ID_269705187" CREATED="1544146319281" MODIFIED="1544146353308">
+<edge COLOR="#ff0000"/>
+<node TEXT="Demonstrate value to employers" ID="ID_567754990" CREATED="1544146512562" MODIFIED="1544146557684"/>
+<node TEXT="Offer independant services" ID="ID_1618393251" CREATED="1544147194703" MODIFIED="1544147246170"/>
+<node TEXT="Documentation Skillbuilding" ID="ID_1258075714" CREATED="1544146530328" MODIFIED="1544146538757"/>
+<node TEXT="Engineering Skillbuilding" ID="ID_234033574" CREATED="1544146540064" MODIFIED="1544146548165"/>
+<node TEXT="Connections to interesting people" ID="ID_1698433646" CREATED="1544146611465" MODIFIED="1544146619047"/>
+</node>
+<node TEXT="Prospective Employer" POSITION="right" ID="ID_848562835" CREATED="1544146360041" MODIFIED="1544146381439">
+<edge COLOR="#0000ff"/>
+<node TEXT="Projects" ID="ID_1138919411" CREATED="1544146469772" MODIFIED="1544146480717">
+<node TEXT="Want to be able to find relevant projects quickly" ID="ID_1781039877" CREATED="1544146587077" MODIFIED="1544146596262"/>
+<node TEXT="See deliverables/successes" ID="ID_1240225699" CREATED="1544147256458" MODIFIED="1544147296940"/>
+<node TEXT="See thought process and approach" ID="ID_1264133477" CREATED="1544147297763" MODIFIED="1544147308417"/>
+<node TEXT="See base of knowledge" ID="ID_1076301350" CREATED="1544147309048" MODIFIED="1544303001573"/>
+</node>
+<node TEXT="Demonstration of skills" ID="ID_816644597" CREATED="1544146482822" MODIFIED="1544146496008">
+<node TEXT="Technical" ID="ID_865747102" CREATED="1544146865345" MODIFIED="1544146869402"/>
+<node TEXT="Team oriented" ID="ID_1298152749" CREATED="1544146870861" MODIFIED="1544146877401"/>
+<node TEXT="Non-technical" ID="ID_1221247906" CREATED="1544146878118" MODIFIED="1544146881154"/>
+</node>
+<node TEXT="Unique attributes" ID="ID_1227968197" CREATED="1544146496466" MODIFIED="1544146572950">
+<node TEXT="Less common relevant skillsets" ID="ID_816890505" CREATED="1544146835009" MODIFIED="1544146849434"/>
+<node TEXT="Sense of personality" ID="ID_427622748" CREATED="1544146852584" MODIFIED="1544146907373"/>
+<node TEXT="Sense of professionalism" ID="ID_1926405575" CREATED="1544146908197" MODIFIED="1544147184789"/>
+</node>
+<node TEXT="Contact" ID="ID_474074418" CREATED="1544146600234" MODIFIED="1544146606911"/>
+</node>
+<node TEXT="Practical Learner" POSITION="right" ID="ID_175147099" CREATED="1544146383008" MODIFIED="1544146420159">
+<edge COLOR="#00ff00"/>
+<node TEXT="Project Documentation" ID="ID_624413626" CREATED="1544146429606" MODIFIED="1544146446423"/>
+<node TEXT="Lessons and how-tos" ID="ID_1722507730" CREATED="1544146449386" MODIFIED="1544146459332"/>
+<node TEXT="Contact" ID="ID_1631261228" CREATED="1544146607830" MODIFIED="1544146610689"/>
+</node>
+</node>
+</map>
