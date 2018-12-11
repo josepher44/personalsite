aca36bb33450a3f031182a746ff097fa74062334
Joe Gallagher <joedgallagher@yahoo.com>
2018-12-10 21:34:45 -0500

    Javascript 35

--- Learning/Javascript/notes.js
+++ Learning/Javascript/notes.js
@@ -50,17 +50,14 @@ myVar="josepher";
 
 var year = "junior";
 
-if (year=="junior")
-{
+if (year=="junior") {
     myVar="Gallagher"
 
 }
-else if (year=="sophomore")
-{
+else if (year=="sophomore") {
     myVar="Joseph"
 }
-else
-{
+else {
     myVar="Joe"
 }
 
@@ -88,8 +85,7 @@ console.log(myVar);
 
 var links = document.getElementsByTagName("a");
 
-for(i = 0; i < links.length; i++)
-{
+for(i = 0; i < links.length; i++) {
     //Concatenate number to link to give name
     //Class name modifies HTML to give them a class
     //Can use javascript to edit base HTML
@@ -104,8 +100,7 @@ for(i = 0; i < links.length; i++)
 
 //Return is same as C
 
-function getAverage(a,b)
-{
+function getAverage(a,b) {
     var average = (a + b)/2;
     console.log(average);
     return average;
@@ -117,7 +112,8 @@ function getAverage(a,b)
 //No compilation or syntax validation
 
 //Variables at global level are defined on 0 level indent
-//Variables declared inside function
+//Variables declared inside function are local
+//Unlike java, block-level variables are not limited to block scope. Variables in function are function-scope, no matter where declared.
 //As usual, don't actually use global variables
 
 //Math object is used for advanced math functions, kinda like numpy
@@ -138,3 +134,137 @@ with(Math)
 //Quotes can be overridden by using escape sequences, ie \'
 
 var myString = 'I\m a "correctly formatted" string';
+
+//String has a bunch of manipulation methods
+//When var type has been explicitly defined, brackets is good about context suggestions
+
+//Strings can be directly compared using == or === operator
+
+//Substrings done via myString.slice(), goes begin/end or begin-to end
+
+//split(",") outputs an array of the string, split at each instance of deliminator
+//Arrays seem like a bigger deal in javascript, more like c than java
+
+//Ternary operators are a thing. Shorthand version of conditional
+//(condition<somevalue) ? "Return if true": Return if  false
+
+//Arrays work the good old fashoned[] way, written and read like gml
+//Arrays can have mixed datatypes. Potential for complex state arrays like rbe2001. Oh god, don't do anything like RBE2001's code. That's gonna be fun to clean for release...
+
+var myArray = Array(5) // initializes array
+//Or, var myArray = [] for empty, indefinite array
+
+//Bunch of array methods, pop, push, sort, etc
+//Mixed sort goes by type, then a subsort
+
+//Oh btw, its all oject oriented!
+"string".split(2);
+//That's valid, performing a dot function call direct on a primative. Cool!
+
+//Instantiating an object, stored to variables
+//Java: Car car = new Car()
+//Javascript: var myCar = new Car()
+//Dot notation is always used
+//method is a function associated with a certain object
+//Local variables of an object are accessed directly, not through getters/setters
+
+//Functions are values which can be stored, just like numbers
+//var foo = function foo(){}
+
+//Always declare all variables at top of function, since that's where scope is
+
+//Always, always K&R in javascript, not Allman, because of implicit semicolon errors
+
+var num = 8;
+num.toString();
+
+//Object time. Also known as, Dillo time!
+var dillo = new Object();
+
+//BIG DIFFERENCE: Variables are dynamically added to the object via dot notation
+dillo.length = 7;
+dillo.dead = false;
+
+//Functions are defined the same way, by creating function()
+
+dillo.hitWithTruck = function(){
+    this.dead = true;
+    this.length += 1;
+    console.log(this);
+}
+
+//Can also be defined via a shorthand -- this is preferred. Fields, INCLUDING FUNCTIONS, are comma seperated. 
+//YOU ARE GONNA FORGET THE COMMA ALL THE TIME
+//DON'T FORGET THE COMMAS
+//Parameters can be added during function creation
+var dillo2 = {
+
+    length: 8, 
+    dead: false, 
+    hitWithTruck: function(){
+        this.dead = true;
+        this.length += 1;
+        console.log(this);
+    },
+
+    feedFood: function(amount) {
+        this.length += amount;
+    }
+};
+
+//Constructors are a better way for instanstiable classes
+
+var dilloClass = function(length, dead){
+    this.length = length;
+    this.dead = dead;
+    this.hitWithTruck = function(){
+        this.dead = true;
+        this.length += 1;
+        console.log(this);
+    };  //Note semicolon, you're assigning a variable to be equal to a function
+    //YOU CAN ASSIGN A VARIABLE EQUAL TO A FUNCTION!! SWEET RACKET!
+    this.feedFood = function(amount) {
+        this.length += amount;
+    };
+}
+
+//Date, random useful object -- constructor returns current date
+var date = new Date();
+
+//Or, with a past date. Months index from zero, which sucks
+var birthday = new Date(1991, 9, 4);
+
+//Various methods
+console.log(date.getHours());
+
+//Dates need to be compared with .getTime()
+//Complex Object instances are not equal, even if all fields are same
+
+
+//THE DOM
+
+//Used to add, remove, or change HTML
+//Represents HTML as a xml-type format
+//Every element/tag is an object
+//Methods and properties will be called on those tags
+//Typical HTML form means it takes on a tree form, with elements being nodes
+//Element is one node, internal text is a node, attributes are nodes
+
+//DOM tree starts with "document." Then further traversed with dot notation. document.body. ...
+//Also have methods to generate arrays of bunches of elements, for example all classes with certain class
+
+//document.getElementsBy... is standard form for all elements
+var allPs = document.getElementsByTagName("p");
+var allContent = document.getElementsByClassName("content");
+
+//Can then use array notation to get specific element in, say, a list of divs
+
+//Id is unique, therefore element rather than elements
+document.getElementById("").;
+
+//Use .innerHTML to overwrite all internal HTML
+//Or, .textContent to overwrite the text only
+
+//GetAttribute() takes argument of attribute name, returns value
+//SetAttribute takes argument of 
+
---
+++ Projects.mm
@@ -0,0 +1,116 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Projects" FOLDED="false" ID="ID_253497212" CREATED="1544304088156" MODIFIED="1544304108260" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Freshman" POSITION="right" ID="ID_1764074468" CREATED="1544304115500" MODIFIED="1544304122231">
+<edge COLOR="#ff0000"/>
+<node TEXT="A Term" ID="ID_1094106700" CREATED="1544305333908" MODIFIED="1544305343400">
+<node TEXT="PH1111" ID="ID_911775803" CREATED="1544305388070" MODIFIED="1544305391737">
+<node TEXT="Example Calculations" ID="ID_206276022" CREATED="1544305421890" MODIFIED="1544305427766">
+<node TEXT="Concepts to show" ID="ID_448852501" CREATED="1544305432842" MODIFIED="1544305634988"/>
+<node TEXT="Tools Required" ID="ID_1931296639" CREATED="1544305489214" MODIFIED="1544305642203">
+<node TEXT="Textbook" ID="ID_1472145772" CREATED="1544305644283" MODIFIED="1544305647468"/>
+<node TEXT="Matlab or Python" ID="ID_1115989206" CREATED="1544305647846" MODIFIED="1544305673820"/>
+</node>
+</node>
+</node>
+<node TEXT="MA1023" ID="ID_440369822" CREATED="1544305393830" MODIFIED="1544305396287"/>
+<node TEXT="ME1800" ID="ID_1379477808" CREATED="1544305396518" MODIFIED="1544305418379"/>
+</node>
+<node TEXT="B Term" ID="ID_1809282531" CREATED="1544305344869" MODIFIED="1544305349196">
+<node TEXT="PH1121" ID="ID_566724125" CREATED="1544305570326" MODIFIED="1544305573862">
+<node TEXT="Example Calculations" ID="ID_1308822056" CREATED="1544305421890" MODIFIED="1544305427766">
+<node TEXT="Textbook" ID="ID_1685628980" CREATED="1544305432842" MODIFIED="1544305483053"/>
+<node TEXT="" ID="ID_271124337" CREATED="1544305489214" MODIFIED="1544305489214"/>
+</node>
+</node>
+</node>
+<node TEXT="C Term" ID="ID_57281916" CREATED="1544305349840" MODIFIED="1544305351626"/>
+<node TEXT="D Term" ID="ID_1103916235" CREATED="1544305352061" MODIFIED="1544305354144"/>
+<node TEXT="E Term" ID="ID_1223305437" CREATED="1544305354383" MODIFIED="1544305356487"/>
+</node>
+<node TEXT="Sophomore" POSITION="right" ID="ID_566380453" CREATED="1544304123095" MODIFIED="1544304125066">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="Junior" POSITION="right" ID="ID_1056098663" CREATED="1544304125764" MODIFIED="1544304127380">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="2014/2015" POSITION="right" ID="ID_941717451" CREATED="1544304127501" MODIFIED="1544304141629">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="2015/2016" POSITION="right" ID="ID_1572146771" CREATED="1544304142145" MODIFIED="1544304148231">
+<edge COLOR="#00ffff"/>
+</node>
+<node TEXT="2016/2017" POSITION="right" ID="ID_1755562602" CREATED="1544304148559" MODIFIED="1544304155369">
+<edge COLOR="#7c0000"/>
+</node>
+<node TEXT="2017/2018" POSITION="right" ID="ID_1399006602" CREATED="1544304155957" MODIFIED="1544304164523">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="2018/2019" POSITION="right" ID="ID_1719818583" CREATED="1544304164941" MODIFIED="1544304167681">
+<edge COLOR="#007c00"/>
+</node>
+</node>
+</map>
